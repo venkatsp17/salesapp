d47ca0f2ae436a60b92a586b4b80d8152ac37c9b
venkatsp <venkatsp2002@gmail.com>
2022-09-26 22:23:57 +0530

    COMMIT - 5

--- lib/FRONTEND/NewCustomer/stepper3.dart
+++ lib/FRONTEND/NewCustomer/stepper3.dart
@@ -1,5 +1,178 @@
 import 'package:flutter/material.dart';
 
-Widget stepper3(){
-  return Container();
+
+
+class stepper3 extends StatefulWidget {
+  const stepper3({Key? key}) : super(key: key);
+
+  @override
+  State<stepper3> createState() => _stepper3State();
 }
+
+class _stepper3State extends State<stepper3> {
+
+  final TextEditingController _openbal = TextEditingController();
+  final TextEditingController _name = TextEditingController();
+  final TextEditingController _phno = TextEditingController();
+
+  String selectedValue = "Select Payment method";
+
+  List<String> payments = ['Select Payment method','Cash', 'Online Payment', 'UPI'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            width: 160,
+            child: Text(
+              "Other Details",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 20,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(
+                width: 90,
+                child: Text(
+                  "Opening Balance",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 250,
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      fillColor: Colors.white70,
+                      filled: true,
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: _openbal,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(
+                width: 90,
+                child: Text(
+                  "Payment Terms",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                height: 60,
+                width: 250,
+                child: DropdownButton(
+                  isExpanded: true,
+                  value: selectedValue,
+                  icon: const Icon(Icons.keyboard_arrow_down),
+                  items: payments.map((String item) {
+                    return DropdownMenuItem(
+                      value: item,
+                      child: Text(item),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedValue = newValue!;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 40,),
+          const SizedBox(
+            width: 160,
+            child: Text(
+              "Contact Details",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 20,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(
+                width: 90,
+                child: Text(
+                  "Name",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 250,
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      fillColor: Colors.white70,
+                      filled: true,
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: _name,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(
+                width: 90,
+                child: Text(
+                  "Mobile No.",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 250,
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      fillColor: Colors.white70,
+                      filled: true,
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: _phno,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
